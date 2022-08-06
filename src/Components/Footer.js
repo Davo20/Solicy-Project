2898aeb8f3665b47377daab8b20276e755f30874
Davo20 <94475388+Davo20@users.noreply.github.com>
2022-08-06 19:30:02 +0300

    Delete Footer.js

--- src/Components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer(){
-    return(
-        <footer>
-            <span>The given project performed David Balabekyan</span>
-        </footer>
-    )
-}
